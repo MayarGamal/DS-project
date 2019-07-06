c279a5a50b6a1bd871eb0841d281d3ab3ad3da47
sara <36546098+SaraSafwat@users.noreply.github.com>
2019-07-06 10:23:45 +0200

    Add files via upload

---
+++ Graph-GUI/Graph-GUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_paint(object sender, PaintEventArgs e)
+        {
+            var Nodes = 4;
+            var edges = 5;
+            Graphics l = e.Graphics;
+            Pen p = new Pen(Color.Black, 2);
+            l.DrawEllipse(p, 50,50, 50, 50);
+            l.DrawLine(p, 70 , 35, 150, 60);
+            l.Dispose();
+        }
+    }
+}
